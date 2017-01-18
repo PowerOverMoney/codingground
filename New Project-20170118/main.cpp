e128a5360ff9dfbcae885bde5c74d4bbff5919f3
PowerOverMoney <mehslambo@gmail.com>
2017-01-18 13:29:59 +0200

    Created by Coding Ground Client

---
+++ New Project-20170118/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+    int f;
+    int g;
+    int h;
+    int i;
+    int j;
+     
+    a = 1;
+    b = 2;
+    c = 3;
+    d = 4;
+    e = 5;
+    f = 6;
+    g = 7;
+    h = 8;
+    i = 9;
+    j = 10;
+    
+    
+   cout << a << ","  << b << " Bumba ir man! "
+   << c << ","   << d << " Noķēru gan! " 
+   << e << ","  << f << " Paceļam! " 
+   << g << ","  << h << " Noliekam! "
+   << i << ","  << j << " Un vēlvienu reiz! " <<   endl; 
+   
+   return 0;
+}
+
+
