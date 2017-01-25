ee22c72691fbc60b5c2546de6b11bd448ae4b505
PowerOverMoney <mehslambo@gmail.com>
2017-01-25 14:02:42 +0200

    Created by Coding Ground Client

---
+++ New Project-20170125/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+int x;
+int c;
+int a;
+
+   cout << "ievadi x" << endl;
+   cin >> x;
+   cout << "ievadi c" << endl;
+   cin >> c;
+
+   a = x + c;
+   cout << "x un c summa ir " << a << endl;
+   return 0;
+}
+
+
